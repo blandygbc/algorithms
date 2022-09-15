c1a2c18ee3e94bdd5cedb9184e24415e83354798
Gabriel Blandy <gabriel_blandy@hotmail.com>
2022-09-15 15:15:27 -0300

    Improve binarysearch loop

--- src/com/blandygbc/algorithms/BinarySearch.java
+++ src/com/blandygbc/algorithms/BinarySearch.java
@@ -6,32 +6,29 @@ public class BinarySearch {
 
     static Logger logger = Logger.getLogger(BinarySearch.class.getName());
 
-    public static void findNumberInArray(Integer[] numbers, Integer target) {
+    public static String findNumberInArray(Integer[] numbers, Integer target) {
         Integer min = 0;
         Integer max = numbers.length - 1;
         Integer mid = 0;
-        boolean search = true;
-        while (search) {
+        StringBuilder stringBuilder = new StringBuilder("The number " + target);
+        while (max > min) {
             mid = (min + max) / 2;
-            if (max < min) {
-                logger.info("The number " + target + " was not found");
-                search = false;
-            } else if (numbers[mid].equals(target)) {
-                logger.info("The number " + target + " was found at position " + mid);
-                search = false;
+            if (numbers[mid].equals(target)) {
+                return stringBuilder.append(" was found at index " + mid).toString();
             } else if (numbers[mid] < target) {
                 min = mid + 1;
             } else if (numbers[mid] > target) {
                 max = mid - 1;
             }
         }
-
+        return stringBuilder.append(" was not found").toString();
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Integer[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
                 97 };
-        findNumberInArray(primes, 5);
+        String result = findNumberInArray(primes, 67);
+        logger.info(result);
     }
 
 }
